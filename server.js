b80446ab19deb573f9d2555cc67d581a1eaf3779
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-08-02 20:15:09 -0700

    added login functionailty

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
@@ -15,6 +19,18 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
